f466d56af4940b93161180c6e4ccd16310c76fbe
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-13 10:34:19 +0000

    [finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,12 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to'/top'
+  end
+
   def index
   end
 
@@ -10,5 +16,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new
-  post 'lists/new' => 'lists#create'
+  get 'lists/new'
+  post 'lists' => 'lists#create'
   
   get 'lists/index'
   get 'lists/show'
