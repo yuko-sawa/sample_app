3c2534239156321d9db4d057ad814d2bc2e14d7f
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-15 07:39:43 +0000

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  
-  get 'lists/index'
-  get 'lists/show'
+  get 'lists' => 'lists#index'
   get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  resources :lists
+  # .../lists/1や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 end
