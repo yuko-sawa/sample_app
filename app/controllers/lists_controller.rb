0944de98c052cf042ba31239150760f6c2d20ca7
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-18 02:45:51 +0000

    in frash

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,10 +5,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    if@list.save
+    if @list.save
+     flash[:notice] = "投稿に成功しました"
      redirect_to list_path(@list.id)
     else
-     render :new
+      render :new
     end
   end
 
